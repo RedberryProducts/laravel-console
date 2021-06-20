c288a83df5b10fe5a4652f32431049b7e3f477e3
giunashvili <giorgi.official@yahoo.com>
2021-06-20 21:36:51 +0400

    add: cache manager

---
+++ CacheManager.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Redberry\LaravelConsole;
+
+use Illuminate\Support\Facades\Cache;
+
+class CacheManager
+{
+    const LOG_KEY = 'laravel-console.logs';
+
+    /**
+     * Clear previous logs from active bus.
+     */
+    public static function clear(): void
+    {
+        Cache::put(self::LOG_KEY, []);
+    }
+
+    /**
+     * Add log record into the cache.
+     */
+    public static function add(array $record): void
+    {
+        $logRecords = Cache::get(self::LOG_KEY, []);
+        $logRecords[] = $record;
+        Cache::put(self::LOG_KEY, $logRecords);
+    }
+
+    /**
+     * Remembered log records.
+     */
+    public static function logs(): array
+    {
+        return Cache::get(self::LOG_KEY, []);
+    }
+}
